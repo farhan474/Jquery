3fb7e6499946441f7b47e8b29e69d84b2b7ae43d
farhan <farhanmohammad8214@gmail.com>
2023-03-28 23:46:36 -0400

    errors fixed

--- Part2/index.js
+++ Part2/index.js
@@ -5,6 +5,14 @@
 $('#submit').on('click', (e)=>{
 	e.preventDefault()
 	const $ol = $("form ol")
-	const $newLi = $(`<li> Title:${$('#movie').val()} Rateing:${$('#rating').val()}</li>`).append($('<button> delete me </button>').on('click',(e)=> {e.target.parent().remove()}))
+	const $newLi = $(`<li> Title:${$('#movie').val()} Rateing:${$('#rating').val()}</li>`).append($('<button> delete me </button> on clicn').click( function(e){
+	   e.preventDefault();
+	  let p = e.target.parentNode;
+	  p.remove();
+	//    console.log(p);
+	//    $(e.target).parent().remove();
+	}
+		)
+	)
 	$ol.append($newLi)
 })
